9bb179907c4c8dfc0b8782cf6e7ac38a362821d6
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-10-28 22:21:00 +0530

    questions

--- 5. Write a program to input 10 numbers in an array and count how many are even and odd.cpp
+++ 5. Write a program to input 10 numbers in an array and count how many are even and odd.cpp
@@ -3,11 +3,10 @@ using namespace std;
 int main() {
     int a[10], i, e = 0, o = 0;
     cout << "Enter 10 numbers:\n";
-    for(i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         cin >> a[i];
-        if(a[i] % 2 == 0) e++; 
-        else o++;
+        if (a[i] % 2 == 0) e++; else o++;
     }
-    cout << "Even: " << e << ", Odd: " << o;
+    cout << "Even: " << e << ", Odd: " << o << endl;
     return 0;
 }
