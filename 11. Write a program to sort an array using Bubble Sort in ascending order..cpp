8776fd165d58cf995d4c5a45b85fca382f58814d
Vishal Puri <89836637+iamvishalpuri@users.noreply.github.com>
2025-10-31 23:17:01 +0530

    Update 11. Write a program to sort an array using Bubble Sort in ascending order..cpp

--- 11. Write a program to sort an array using Bubble Sort in ascending order..cpp
+++ 11. Write a program to sort an array using Bubble Sort in ascending order..cpp
@@ -6,6 +6,7 @@ int main() {
     cin >> n;
     int a[100];
     cout << "Enter elements:\n";
+    for (i = 0; i < n; i++)
         cin >> a[i];
     for (i = 0; i < n - 1; i++)
         for (j = 0; j < n - 1 - i; j++)
